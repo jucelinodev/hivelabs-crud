97a1c65d71182bca5e43a10e7309dcd264ffec55
Jucelino Novais <jucelinodev@gmail.com>
2021-08-06 18:07:30 -0300

    config eslint and prettier

--- src/index.ts
+++ src/index.ts
@@ -16,5 +16,5 @@ const APP_HOST = process.env.APP_HOST || 'http://localhost'
 const PORT = process.env.PORT || 3333
 
 app.listen(PORT, () =>
-  console.log(`Servidor executando em ${APP_HOST}:${PORT}`)
+  console.log(`Servidor executando em ${APP_HOST}:${PORT}`),
 )
